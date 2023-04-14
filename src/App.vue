5cfa5aabfc0b0b15f93dbccfb9e596b8ed06ac80
MartaMartinez <marmarbcn@gmail.com>
2023-04-14 18:01:53 +0200

    Done with bonus

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div class="app">
+  <div class="app container">
     <h1>IronContacts</h1>
-    <button @click="addRandomContact()"> Add contact </button>
-    <button>Sort by popularity</button>
-    <button>Sort by name</button>
-    <table class=" table table-hover">
-      <thead class="table-dark">
-        <tr>
+    <div class="btn-container">
+      <button type="button" class="btn btn-outline-secondary" @click="addRandomContact()"> Add contact </button>
+      <button type="button" class="btn btn-outline-secondary" @click="byPopularity()">Sort by popularity</button>
+      <button type="button" class="btn btn-outline-secondary" @click="byName()">Sort by name</button>
+    </div>
+    <table class=" table table-hover table-bordered table align-middle text-center">
+      <thead>
+        <tr class="thead">
 
           <th scope="col">Picture</th>
           <th scope="col">Name</th>
           <th scope="col">Popularity</th>
           <th scope="col">Won an Oscar</th>
           <th scope="col">Won an Emmy</th>
+          <th scope="col"></th>
         </tr>
       </thead>
-      <tbody>
+      <tbody class="table-group-divider">
         <tr v-for="item in contacts" :key="item.id">
 
           <td><img :src="item.pictureUrl" alt="{{ item.name }}" style="width: 50px"></td>
           <td>{{ item.name }}</td>
           <td>{{ item.popularity.toFixed(2) }}</td>
-          <td v-if="item.wonOscar === true">✌️</td>
-          <td v-if="item.wonEmmy === true">✌️</td>
+          <td><span v-show="item.wonOscar === true">✌️</span></td>
+          <td><span v-show="item.wonEmmy === true">✌️</span></td>
+          <td><button type="button" class="btn btn-outline-danger btn-sm" @click="deleteBtn(`${item.id}`)">Delete
+              contact</button></td>
+
         </tr>
       </tbody>
     </table>
@@ -42,11 +48,49 @@ const addRandomContact = () => {
   return contacts.value.push(randomContact);
 }
 
-// const byPopularity = () => {
-//   const popularity = contacts.map((contact) => contact.popularity);
-//   console.log(popularity)
-// }
-// byPopularity();
+const byPopularity = () => {
+  const popularityOrder = contacts.value.sort((a, b) => b.popularity - a.popularity);
+  console.log(popularityOrder);
+}
+
+const byName = () => {
+  const alphabeticOrder = contacts.value.sort((a, b) => {
+    a = a.name.toLocaleLowerCase();
+    b = b.name.toLocaleLowerCase();
+    if (a === b) {
+      return 0;
+    }
+    if (a < b) {
+      return -1;
+    }
+    else { return 1; }
+  }
+  )
+};
+
+const deleteBtn = () => {
+  const removed = contacts.value.splice(0, 1);
+  console.log(removed);
+  console.log(contacts);
+  console.log(restContacts);
+  return restContacts.value.push(removed.value)
+}
 </script>
 
-<style></style>
+<style scoped>
+.container {
+  max-width: 600px;
+  text-align: center;
+}
+
+.thead {
+  text-align: center;
+  justify-content: center;
+  background-color: #ffccb6;
+  color: #ff968a;
+}
+
+img {
+  border-radius: 50px;
+}
+</style>
