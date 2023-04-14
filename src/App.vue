e7aee07b1e734536d9a148ea26a3fe707b1e3dbb
MartaMartinez <marmarbcn@gmail.com>
2023-04-14 11:52:56 +0200

    Iterations 1 - 2 - 3 done!

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <div class="app">
+    <h1>IronContacts</h1>
+    <button @click="addRandomContact()"> Add contact </button>
+    <button>Sort by popularity</button>
+    <button>Sort by name</button>
+    <table class=" table table-hover">
+      <thead class="table-dark">
+        <tr>
+
+          <th scope="col">Picture</th>
+          <th scope="col">Name</th>
+          <th scope="col">Popularity</th>
+          <th scope="col">Won an Oscar</th>
+          <th scope="col">Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in contacts" :key="item.id">
+
+          <td><img :src="item.pictureUrl" alt="{{ item.name }}" style="width: 50px"></td>
+          <td>{{ item.name }}</td>
+          <td>{{ item.popularity.toFixed(2) }}</td>
+          <td v-if="item.wonOscar === true">✌️</td>
+          <td v-if="item.wonEmmy === true">✌️</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import contactsData from './contacts.json';
+import { ref } from 'vue';
+
+let contacts = ref(contactsData.slice(0, 5));
+let restContacts = ref(contactsData.slice(6));
+
+const addRandomContact = () => {
+  let randomContact = restContacts.value[Math.floor(Math.random() * (restContacts.value.length - 1))];
+  console.log(randomContact);
+  return contacts.value.push(randomContact);
+}
+
+// const byPopularity = () => {
+//   const popularity = contacts.map((contact) => contact.popularity);
+//   console.log(popularity)
+// }
+// byPopularity();
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
