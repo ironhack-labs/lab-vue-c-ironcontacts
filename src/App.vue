4f5319e97acb9514d3d2041a6216e2e6694fab24
Eber Brown <eaba_89@hotmail.com>
2024-03-19 12:55:53 +0100

    first commit and iteration 1 for the lab

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,66 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
+import { ref } from "vue";
+import Contacts from "./contacts.json"
+
+const contacts = ref(Contacts);
+
+function fiveContacts(contactsJson) {
+  contacts.value = contactsJson.slice(0, 5);
+}
+
+fiveContacts(Contacts);
+
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<template>
+  <!-- <ul class="contacts-container" v-for="(contact, index) in contacts" :key="index">
+    <li>{{ contact.name }}</li>
+  </ul> -->
+
+  <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+    </thead>
+    <tbody id="contactList">
+      <!-- Table rows will be dynamically generated here -->
+      <tr v-for="(contact, index) in contacts" :key="index">
+        <td>
+          <img :src="contact.pictureUrl" alt="" srcset="">
+        </td>
+        <td>
+          <p>{{ contact.name }}</p>
+        </td>
+        <td>
+          <p>{{ contact.popularity.toFixed(2) }}</p>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<style scoped>
+table {
+  width: 50%;
+  border-collapse: collapse;
+}
+
+th,
+td {
+  padding: 8px;
+  text-align: left;
+  border-bottom: 1px solid #ddd;
+}
+
+th {
+  background-color: #f2f2f2;
+}
+
+img {
+  max-width: 100px;
+  max-height: 100px;
 }
 </style>
