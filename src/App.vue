78945b2b9f857233b74e9715cf6a65c0c087dbcd
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-21 20:10:38 +0100

    iteration 1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
-<template>
-
-</template>
-
 <script setup>
+import { ref } from "vue";
+import contacts from "@/contacts.json";
+
+const fiveContacts = ref(contacts.slice(0, 5));
+const sortedContacts = ref(contacts.sort((a, b) => b.popularity - a.popularity));
+console.log(sortedContacts);
 </script>
+
+<template>
+  <div id="app">
+    <h1>IronContacts</h1>
+    <table>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+      </tr>
+      <tr v-for="person in fiveContacts" :key="person.id">
+        <td><img :src="person.pictureUrl" class="person-picture"></td>
+        <td>{{ person.name }}</td>
+        <td>{{ person.popularity.toFixed(2) }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
 
 <style scoped>
 #app {
@@ -13,5 +33,12 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.person-picture {
+  width: 50px;
 }
 </style>
