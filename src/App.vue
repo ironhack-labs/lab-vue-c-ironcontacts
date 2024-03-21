1f539b7aec421f3fb29343e08642734c6582ca33
Stefano Romanelli <stefanoromanelli86@gmail.com>
2024-03-21 19:01:24 +0100

    cleanup app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+
 </template>
 
 <script setup>
-import HelloWorld from './components/HelloWorld.vue';
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
