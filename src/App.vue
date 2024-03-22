6cc5e060409b6d3a2056804e6e24e0ae843dd3be
Auba Fuster <aubafp@gmail.com>
2024-03-23 00:39:24 +0100

    finish iteration 1 and 2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
+  <table>
+    <thead>
+      <tr>
+        <th>Picture</th>
+        <th>Name</th>
+        <th>Popularity</th>
+        <th>Won Oscar</th>
+        <th>Won Emmy</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="(con, index) in contact" :key="index">
+        <td><img :src="con.pictureUrl" :alt="con.name"></td>
+        <td>{{ con.name }}</td>
+        <td>{{ con.popularity }}</td>
+        <td v-if="con.wonOscar === true">🏆</td>
+        <td v-if="con.wonEmmy === true">🏆</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
   import { ref } from 'vue';
   import contacts from '@/contacts.json'
 
-  const contact = ref(contacts.slice(0, 5));
-  console.log(contact)
+  const contact = ref(contacts.slice(5, 10));
 
 </script>
 
 <style scoped>
+  img {
+    max-height: 100px;
+  }
 </style>
 
