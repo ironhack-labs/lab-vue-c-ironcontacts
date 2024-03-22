6ebd8dfeed2d455eb1923fa114394bfad8332f8a
Auba Fuster <aubafp@gmail.com>
2024-03-22 21:39:40 +0100

    half of iteration 1 done. Pending template html

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
 </template>
 
 <script setup>
+  import { ref } from 'vue';
+  import contacts from '@/contacts.json'
+
+  const contact = ref(contacts.slice(0, 5));
+  console.log(contact)
+
 </script>
 
 <style scoped>
