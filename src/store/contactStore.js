8835dc699d8e1ea570b83e9fdd5b05d0b3b6ba53
Laura Musso <lauramussopd@gmail.com>
2024-03-26 18:59:38 +0100

    lab done tue

--- src/store/contactStore.js
+++ src/store/contactStore.js
@@ -7,7 +7,7 @@ export const useContactsStore = defineStore('contacts', () => {
     // State
 
     const originalContacts = ref(contacts)
-    
+
     const displayContacts = ref(originalContacts.value.slice(0, 5))
     const sortByPopularity = computed(() => displayContacts.value.sort((a, b) => b.popularity - a.popularity));
     const sortByName = computed(() => displayContacts.value.sort((a, b) => a.name.localeCompare(b.name)));
@@ -18,25 +18,23 @@ export const useContactsStore = defineStore('contacts', () => {
         if (remainingContacts.length > 0) {
           const randomIndex = Math.floor(Math.random() * remainingContacts.length);
           displayContacts.value.push(remainingContacts[randomIndex]);
-        //   console.log("New contact added:", displayContacts.value);
         }
     }
 
     function sortContactsByPopularity() {
-      displayContacts.value = sortByPopularity.value
-    //   console.log("Contacts sorted by popularity:", displayContacts.value);
+      displayContacts.value = sortByPopularity.value;
+
     }
 
     function sortContactsByName() {
-      displayContacts.value = sortByName.value
-    //   console.log("Contacts sorted by name:", displayContacts.value);
+      displayContacts.value = sortByName.value;
+
     }
 
     function deleteContact(contactId) {
       const indexToRemove = displayContacts.value.findIndex(contact => contact.id === contactId);
       if (indexToRemove !== -1) {
         displayContacts.value.splice(indexToRemove, 1);
-        // console.log("Contact deleted:", displayContacts.value);
       }
     }
 
